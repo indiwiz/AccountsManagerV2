b37a7e7bd8c7a7b291bda2157d7677a20234b74d
jaychvn <jaychvn22@gmail.com>
2020-04-06 20:37:58 +0100

    AM-16 Server side validation

--- src/AccountsManager.Web/Models/CompanyRegistrationModel.cs
+++ src/AccountsManager.Web/Models/CompanyRegistrationModel.cs
@@ -4,7 +4,10 @@ namespace AccountsManager.Web.Models
 {
     public class CompanyRegistationModel
     {
-        [StringLength(250), Required]
+        public int Id { get; set; }
+        [StringLength(250, ErrorMessage="Name cannot exceed 250 characters")]
+        [Required (ErrorMessage="Company Name cannot be blank")]
+        [RegularExpression("([a-zA-Z0-9 .&'-]+)", ErrorMessage= "Sprecial characters not allowed")]
         public string CompanyName { get; set; }
     }
 }
